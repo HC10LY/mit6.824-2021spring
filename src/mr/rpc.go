3fffec4e6cf1b71ff910c1034af2af030352babc
hc <599221766@qq.com>
2025-07-27 18:32:28 +0800

    完成lab1,通过测试

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -24,6 +24,28 @@ type ExampleReply struct {
 
 // Add your RPC definitions here.
 
+// MapReduce 任务请求参数
+type TaskRequestArgs struct {
+	WorkerId int // 暂时不需要，可以扩展
+}
+
+// MapReduce 任务分配返回
+type TaskReply struct {
+	TaskType   string   // "Map", "Reduce", "Wait", "Exit"
+	FileNames  []string // Map任务输入文件名列表，Reduce任务中间文件名列表
+	TaskNumber int      // 任务编号（map或reduce任务号）
+	NReduce    int      // Reduce任务总数
+}
+
+// 上报完成任务的参数
+type TaskCompleteArgs struct {
+    TaskType   string // "Map" or "Reduce"
+    TaskNumber int    // 完成的任务号
+}
+
+// 上报完成任务的回复（可以不需要特别内容）
+type TaskCompleteReply struct{}
+
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -4,6 +4,11 @@ import "fmt"
 import "log"
 import "net/rpc"
 import "hash/fnv"
+import "time"
+import "encoding/json"
+import "io/ioutil"
+import "os"
+import "sort"
 
 
 //
@@ -28,10 +33,167 @@ func ihash(key string) int {
 //
 // main/mrworker.go calls this function.
 //
+
+// doMapTask 执行 Map 任务，读取输入文件内容，调用 mapf，
+// 将结果分桶写入多个中间文件 (mr-X-Y)，X是map任务号，Y是reduce任务号
+func doMapTask(reply TaskReply, mapf func(string, string) []KeyValue) {
+	fmt.Printf("doMapTask: task %d start\n", reply.TaskNumber)
+
+	if len(reply.FileNames) == 0 {
+		log.Printf("doMapTask: no input files for map task %d", reply.TaskNumber)
+		return
+	}
+
+	filename := reply.FileNames[0] // Map任务只对应一个输入文件
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatalf("doMapTask: cannot read %v", filename)
+	}
+
+	kva := mapf(filename, string(content))
+
+	// 创建 nReduce 个文件，对应 nReduce 个 reduce 任务
+	nReduce := reply.NReduce
+
+	// 为每个 reduce 分区创建一个编码器，写入对应中间文件
+	encoders := make([]*json.Encoder, nReduce)
+	files := make([]*os.File, nReduce)
+	for i := 0; i < nReduce; i++ {
+		intermediateFile := fmt.Sprintf("mr-%d-%d", reply.TaskNumber, i)
+		f, err := os.Create(intermediateFile)
+		if err != nil {
+			log.Fatalf("doMapTask: cannot create file %v", intermediateFile)
+		}
+		files[i] = f
+		encoders[i] = json.NewEncoder(f)
+	}
+
+	// 按 key hash 分桶写入对应文件
+	for _, kv := range kva {
+		bucket := ihash(kv.Key) % nReduce
+		err := encoders[bucket].Encode(&kv)
+		if err != nil {
+			log.Fatalf("doMapTask: encode error: %v", err)
+		}
+	}
+
+	// 关闭所有文件
+	for _, f := range files {
+		f.Close()
+	}
+
+	// TODO: 任务完成后，向 Coordinator 汇报（需要你实现 RPC 调用）
+	fmt.Printf("doMapTask: task %d done\n", reply.TaskNumber)
+	doneArgs := TaskCompleteArgs{
+		TaskType:   "Map",
+		TaskNumber: reply.TaskNumber,
+	}
+	doneReply := TaskCompleteReply{}
+	call("Coordinator.ReportTaskDone", &doneArgs, &doneReply)
+}
+
+// doReduceTask 执行 Reduce 任务，读取所有对应的中间文件，
+// 解码合并 KeyValue，排序，调用 reducef，输出最终结果文件 mr-out-X
+func doReduceTask(reply TaskReply, reducef func(string, []string) string) {
+	fmt.Printf("doReduceTask: task %d start\n", reply.TaskNumber)
+
+	nReduce := reply.NReduce
+	taskNum := reply.TaskNumber
+
+	// 读取所有 map 任务生成的对应 reduce 分区的中间文件
+	var kva []KeyValue
+	for mapTaskNum := 0; mapTaskNum < nReduce; mapTaskNum++ {
+		intermediateFile := fmt.Sprintf("mr-%d-%d", mapTaskNum, taskNum)
+		file, err := os.Open(intermediateFile)
+		if err != nil {
+			// 如果文件不存在，跳过（可能某些任务不产生该文件）
+			continue
+		}
+		dec := json.NewDecoder(file)
+		for {
+			var kv KeyValue
+			if err := dec.Decode(&kv); err != nil {
+				break
+			}
+			kva = append(kva, kv)
+		}
+		file.Close()
+	}
+
+	// 按 Key 排序
+	sort.Slice(kva, func(i, j int) bool {
+		return kva[i].Key < kva[j].Key
+	})
+
+	// 输出文件
+	outFileName := fmt.Sprintf("mr-out-%d", taskNum)
+	outFile, err := os.Create(outFileName)
+	if err != nil {
+		log.Fatalf("doReduceTask: cannot create output file %v", outFileName)
+	}
+	defer outFile.Close()
+
+	// 按 Key 分组，调用 reducef 写结果
+	i := 0
+	for i < len(kva) {
+		j := i + 1
+		for j < len(kva) && kva[j].Key == kva[i].Key {
+			j++
+		}
+
+		values := []string{}
+		for k := i; k < j; k++ {
+			values = append(values, kva[k].Value)
+		}
+
+		output := reducef(kva[i].Key, values)
+		fmt.Fprintf(outFile, "%v %v\n", kva[i].Key, output)
+
+		i = j
+	}
+
+	fmt.Printf("doReduceTask: task %d done\n", taskNum)
+
+	// TODO: 任务完成后，向 Coordinator 汇报（需要你实现 RPC 调用）
+	doneArgs := TaskCompleteArgs{
+		TaskType:   "Reduce",
+		TaskNumber: reply.TaskNumber,
+	}
+	doneReply := TaskCompleteReply{}
+	call("Coordinator.ReportTaskDone", &doneArgs, &doneReply)
+}
+
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
 	// Your worker implementation here.
+
+	for {
+		// 1. 请求任务
+		args := TaskRequestArgs{}
+		reply := TaskReply{}
+		ok := call("Coordinator.AssignTask", &args, &reply)
+		if !ok {
+			// RPC调用失败，可能Coordinator已退出，Worker也退出
+			log.Println("Coordinator not responding, exiting")
+			return
+		}
+
+		switch reply.TaskType {
+		case "Map":
+			doMapTask(reply, mapf)
+		case "Reduce":
+			doReduceTask(reply, reducef)
+		case "Wait":
+			time.Sleep(time.Second) // 等待1秒再请求
+		case "Exit":
+			log.Println("All tasks done, worker exiting")
+			return
+		default:
+			log.Printf("Unknown task type: %v", reply.TaskType)
+			time.Sleep(time.Second)
+		}
+	}
 
 	// uncomment to send the Example RPC to the coordinator.
 	// CallExample()
